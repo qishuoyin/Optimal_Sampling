86fd2259340a0b6f6c43e1d44eee405857c5742e
Qishuo Yin <qy1448@princeton.edu>
2025-03-30 15:16:30 -0400

    Create data_sample_simulation_example.sh

---
+++ exp/exp1_simulation/data_generate_script/data_sample_simulation_example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=data_sample_simulation_example      # create a short name for your job
+#SBATCH --nodes=1                                      # node count
+#SBATCH --ntasks=1                                     # total number of tasks across all nodes
+#SBATCH --cpus-per-task=30                             # cpu-cores per task (>1 if multithread tasks)
+#SBATCH --mem-per-cpu=16G                              # memory per cpu-core (4G is default)
+#SBATCH --time=24:00:00                                # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin                              # send email when process begins
+#SBATCH --mail-type=fail                               # send email if job fails
+#SBATCH --mail-type=end                                # send email when job ends
+#SBATCH --mail-user=qy1448@princeton.edu
+
+module purge
+module load anaconda3/2024.2
+conda activate optimal_sampling_env
+
+Rscript data_sample_simulation_example.R
