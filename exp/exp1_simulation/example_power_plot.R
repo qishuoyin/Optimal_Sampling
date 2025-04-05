7d6fbb57f09e57b93bd45797ade4f68919c14e7a
Qishuo Yin <qy1448@princeton.edu>
2025-04-05 17:17:13 -0400

    modified:   exp/exp1_simulation/example_power_plot.R modified:   exp/exp1_simulation/example_power_plot_outcome_10_I_200_method_select.png

--- exp/exp1_simulation/example_power_plot.R
+++ exp/exp1_simulation/example_power_plot.R
@@ -8,14 +8,13 @@ library(ggplot2)
 current_dir <- getwd()
 
 # Load dataset
-# data_dir <- file.path(current_dir, "evaluation_result", "power_outcome_10_I_200_method_select.csv")
 data_dir <- file.path(current_dir, "evaluation_result", "example_power_outcome_10_I_200_method_select.csv")
 
 # Read the dataset without header
 df <- read.csv(data_dir, header = FALSE, stringsAsFactors = FALSE)
 
 # Extract the second row (since the first row contains column names)
-row2 <- as.numeric(as.character(unlist(df[2, ])))
+row2 <- as.numeric(as.character(unlist(df[2, -ncol(df)])))
 
 # Remove NA values (caused by non-numeric entries)
 row2 <- row2[!is.na(row2)]
@@ -28,7 +27,7 @@ p <- ggplot(row2_df, aes(x = SplitFraction, y = TestPower)) +
   geom_line(color = "blue", size = 1) +  # Line plot
   geom_point(color = "red", size = 2) +  # Points for clarity
   theme_classic() +  # Removes grey background
-  labs(x = "Split fraction", y = "Test power")  # No title
+  labs(x = "Split fraction （%）", y = "Test power")  # No title
 
 # Save the plot in the same directory as the R script
 save_path <- file.path(current_dir, "example_power_plot_outcome_10_I_200_method_select.png")
