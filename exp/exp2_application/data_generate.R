729d2f57fab543f8ea3b0429e4d292e08b559c49
Qishuo Yin <qy1448@princeton.edu>
2025-04-25 15:06:52 -0400

    Update data_generate.R

--- exp/exp2_application/data_generate.R
+++ exp/exp2_application/data_generate.R
@@ -18,12 +18,7 @@ data_control = read.csv(paste(c(current_dir, "data_pair_control.csv"), collapse=
 
 # set directory for generated plasmode datasets
 plasmode_dir = paste(c(current_dir, "data_plasmode"), collapse="/")
-plasmode_dir_sub_1 = paste(c(plasmode_dir, "data_plasmode_0_1"), collapse="/")
-plasmode_dir_sub_2 = paste(c(plasmode_dir, "data_plasmode_0_2"), collapse="/")
-plasmode_dir_sub_3 = paste(c(plasmode_dir, "data_plasmode_0_3"), collapse="/")
 
 # generate plasmode datasets
-generate_plasmode_1 = plasmode_datasets(data_control, plasmode_dir_sub_1, sim_num=1000, effect_ratio=0.1, effect_size_lower=0.05, effect_size_upper=0.2)
-generate_plasmode_2 = plasmode_datasets(data_control, plasmode_dir_sub_2, sim_num=1000, effect_ratio=0.1, effect_size_lower=0.05, effect_size_upper=0.2)
-generate_plasmode_3 = plasmode_datasets(data_control, plasmode_dir_sub_3, sim_num=1000, effect_ratio=0.1, effect_size_lower=0.05, effect_size_upper=0.2)
+generate_plasmode = plasmode_datasets(data_control, plasmode_dir, sim_num=1000, effect_ratio=0.1, effect_size_lower=0.05, effect_size_upper=0.2)
 
