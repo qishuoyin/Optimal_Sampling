f0ff4c1cd37e8ce25482d58390eaa36e34c8bf1f
Qishuo Yin <qy1448@princeton.edu>
2025-04-30 21:00:09 -0400

    Update data_generate.R

--- exp/exp2_application/data_generate.R
+++ exp/exp2_application/data_generate.R
@@ -7,7 +7,7 @@ parent_dir = dirname(getwd())
 parent_parent_dir = dirname(parent_dir)
 
 # import necessary sources
-fun_dir = paste(c(parent_parent_dir, "fun"), collapse="/")
+fun_dir = paste(c(parent_parent_dir, "R"), collapse="/")
 source(paste(c(fun_dir, "fun_plasmode_datasets.R"), collapse="/"))
 
 # set seed for the simulation
@@ -17,7 +17,7 @@ set.seed(2024)
 data_control = read.csv(paste(c(current_dir, "data_pair_control.csv"), collapse="/"))
 
 # set directory for generated plasmode datasets
-plasmode_dir = paste(c(current_dir, "data_plasmode"), collapse="/")
+plasmode_dir = paste(c(current_dir, "data/data_plasmode"), collapse="/")
 
 # generate plasmode datasets
 generate_plasmode = plasmode_datasets(data_control, plasmode_dir, sim_num=1000, effect_ratio=0.1, effect_size_lower=0.05, effect_size_upper=0.2)
