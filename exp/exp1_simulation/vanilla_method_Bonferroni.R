4fa2233cb8f811609923bae2e49c43d8317eb014
Qishuo Yin <qy1448@princeton.edu>
2025-04-30 20:36:35 -0400

    Update vanilla_method_Bonferroni.R

--- exp/exp1_simulation/vanilla_method_Bonferroni.R
+++ exp/exp1_simulation/vanilla_method_Bonferroni.R
@@ -10,7 +10,7 @@ parent_parent_dir = dirname(parent_dir)
 set.seed(2024)
 
 # import necessary sources
-fun_dir = paste(c(parent_parent_dir, "fun"), collapse="/")
+fun_dir = paste(c(parent_parent_dir, "R"), collapse="/")
 source(paste(c(fun_dir, "fun_vanilla_Bonferroni_test.R"), collapse="/"))
 source(paste(c(fun_dir, "fun_power_evaluation.R"), collapse="/"))
 
@@ -21,6 +21,9 @@ I_vec = c(100, 200, 500, 1000) # number of units
 K_vec = c(10, 100, 500, 1000) # number of outcomes
 Gamma_vec = c(1, 1.25, 1.5, 1.75, 2) # design sensitivity
 method = "vanilla_Bonferroni"
+
+
+vanilla_Bonferroni_result_dir = paste(c(current_dir, "output/output_vanilla_Bonferroni_test"), collapse="/")
 
 
 for(i1 in 1:length(K_vec)) {
@@ -44,10 +47,10 @@ for(i1 in 1:length(K_vec)) {
       
       for(t in 1:test_sim) {
         
-        # read data set generated before
+        # read dataset generated before
         sim_info = paste(c("data", "simulation_diff", "outcome", K, "Gamma", Gamma, "I", I, "test_sim", t), collapse="_")
         data_name = paste(c(sim_info, "csv"), collapse=".")
-        data_path = paste(c(paste(c(current_dir, "data_simulation"), collapse="/"), data_name), collapse="/")
+        data_path = paste(c(paste(c(current_dir, "data/data_simulation"), collapse="/"), data_name), collapse="/")
         V = read.csv(data_path)
         
         test_result = Bonferroni_test(Gamma, V)
@@ -57,7 +60,7 @@ for(i1 in 1:length(K_vec)) {
       
       file_info = paste(c("outcome", K, "Gamma", Gamma, "I", I, "method", method), collapse="_")
       Bonferroni_file_name = paste(c(paste(c("Bonferroni_test", file_info), collapse="_"), "csv"), collapse=".")
-      Bonferroni_file_path = paste(c(paste(c(current_dir, "Bonferroni_test"), collapse="/"), Bonferroni_file_name), collapse="/")
+      Bonferroni_file_path = paste(c(vanilla_Bonferroni_result_dir, Bonferroni_file_name), collapse="/")
       
       write.csv(Bonferroni_result, Bonferroni_file_path, row.names = FALSE)
       
@@ -69,7 +72,7 @@ for(i1 in 1:length(K_vec)) {
     }
     
     power_file_name = paste(c(paste(c("power", "outcome", K, "I", I, "method", method), collapse="_"), "csv"), collapse=".")
-    power_file_path = paste(c(paste(c(current_dir, "Bonferroni_test"), collapse="/"), power_file_name), collapse="/")
+    power_file_path = paste(c(vanilla_Bonferroni_result_dir, power_file_name), collapse="/")
     write.csv(power_mat, power_file_path, row.names = FALSE)
     print(paste("K =", K, "; I =", I, "; method =", method))
     
